a307f67597a3eb654266656110a8241ec6f6e97c
Rohit Maurya <rsmart26.rm@gmail.com>
2019-08-19 17:52:02 -0700

    Create kthLargestElement.java

---
+++ MediumProblems/SortingAndSearching/kthLargestElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap=new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            heap.add(nums[i]);
+        }
+        for(int i=k;i<nums.length;i++){
+            if(heap.peek()<nums[i]){
+                heap.poll();
+                heap.add(nums[i]);
+            }
+        }
+        return heap.poll();
+    }
+}
